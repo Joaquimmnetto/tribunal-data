c7a2a0314467b10f374cee4a75da9cefcef1ee77
Joaquim Alvino de Mesquita Neto <joaquimmnetto@gmail.com>
2017-08-04 20:29:22 -0300

    lda_builder now uses corpus_iterator, in order to avoid mutiple file loadings and code simplicity

--- src/python/utils/lda_builder.py
+++ src/python/utils/lda_builder.py
@@ -1,6 +1,7 @@
 from gensim.models import LdaMulticore
 from gensim.matutils import Scipy2Corpus
 
+from corpus_iterator import BowGensimIterator
 import utils
 #import args_proc as args
 from params import args, vecs, clt
@@ -17,20 +18,23 @@ def main():
     num_topics = int(args.get("num_topics", 15))
     analysis = bool(args.get("analysis", False))
 
-    print("Loading bow matrix:")
-    spy_mat, id2word = utils.load_spy_matrix(vecs.bow.mtx.format(0), vecs.bow.vocab)
-    gsm_corpus = Scipy2Corpus(spy_mat.tocsc())
+    print("Loading bow matrix:")    
+    bow_names = (vecs.bow.mtx.format(i) for i in range(0,vecs.n_matrix))
+    gsm_corpus = BowGensimIterator(*bow_names)    
+    id2word = load_obj(vocab)
+    id2word = dict([(i, v) for i, v in enumerate(id2word)
 
     print("Making lda model with first matrix:")
     lda_model = lda_topic_discovery(gsm_corpus, id2word, num_topics)
     del spy_mat,gsm_corpus,id2word
 
-    for i in range(1, vecs.n_matrix):
-        print("Updating model with matrix {0}:".format(i))
-        spy_mat = utils.load_obj(vecs.bow.mtx.format(i))
-        gsm_corpus = Scipy2Corpus(spy_mat.tocsc())
-        lda_model.update(gsm_corpus)
-        del spy_mat,gsm_corpus
+    # for i in range(1, vecs.n_matrix):
+    #     print("Updating model with matrix {0}:".format(i))
+    #     spy_mat = utils.load_obj(vecs.bow.mtx.format(i))
+    #     gsm_corpus = Scipy2Corpus(spy_mat.tocsc())
+    #     lda_model.update(gsm_corpus)
+    #     del spy_mat,gsm_corpus
+    
     lda_model.save(clt.lda.model)
 
 if __name__ == '__main__':
