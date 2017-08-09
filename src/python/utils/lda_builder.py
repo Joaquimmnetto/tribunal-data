75c44769513bcdb7c7d5c4862bff8053e1834fff
Joaquim neto <joaquimmnetto@gmail.com>
2017-08-09 14:07:03 -0300

    mudança de parametros

--- src/python/utils/lda_builder.py
+++ src/python/utils/lda_builder.py
@@ -12,7 +12,7 @@ from params import args, vecs, clt
 def lda_topic_discovery(corpus, id2word, num_topics, multithread):
     #lda_model = LdaMulticore(corpus=corpus, minimum_probability=0, num_topics=num_topics, id2word=id2word, workers=5)
     if multithread:
-      lda_model = LdaMulticore(corpus=corpus, minimum_probability=0, alpha='asymmetric', iterations=100, num_topics=num_topics, id2word=id2word, workers=5)
+      lda_model = LdaMulticore(corpus=corpus, minimum_probability=0, iterations=100, num_topics=num_topics, id2word=id2word, workers=5)
     else:
       lda_model = LdaModel(corpus=corpus, minimum_probability=0, alpha='auto', iterations=100, num_topics=num_topics, id2word=id2word)
     return lda_model
